c68dde0ff66d3c6666f8941cda6dad641ab1c277
tairar <taisaishi0429@gmail.com>
2023-05-07 02:37:18 +0900

    first commit

---
+++ nvim/lua/base.lua
@@ -0,0 +1,24 @@
+-- encoding settings
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+-- appearance
+vim.wo.number = true
+vim.opt.title = true
+vim.opt.showcmd = true
+
+-- editing
+vim.opt.autoindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.ai = true
+vim.opt.si = true
+vim.opt.backspace = indent,eol,start
+vim.opt.showmatch = true
+
+-- searching
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
