2fdf446abe38a599ecc1971ee2f16e4ecb2374ae
tairar <taisaishi0429@gmail.com>
2023-07-04 06:42:47 +0900

    new file: plugin/omnisharp_setup.lua

---
+++ nvim/plugin/omnisharp_setup.lua
@@ -0,0 +1,9 @@
+vim.cmd([[
+let g:ale_sign_error = '•'
+let g:ale_sign_warning = '•'
+let g:ale_sign_info = '·'
+let g:ale_sign_style_error = '·'
+let g:ale_sign_style_warning = '·'
+
+let g:ale_linters = { 'cs': ['OmniSharp'] }
+]])
