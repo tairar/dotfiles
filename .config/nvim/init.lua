d51c038278022bdfdccd5bbe84bdffe4033d2813
tairar <taisaishi0429@gmail.com>
2023-12-06 00:52:38 +0900

    nvim: added settings for windows

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,8 @@
 require('base')
 require('highlights')
 require('setup_plugin')
+
+if vim.fn.has('win64') == 1 then
+	require 'nvim-treesitter.install'.prefer_git = false
+	require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
+end
