218c080dea4f3be0f662414795ec60f00a8c2408
tairar <taisaishi0429@gmail.com>
2024-04-17 17:36:04 +0900

    Changed clipboard settings for wsl

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,11 @@ require('highlights')
 require('setup_plugin')
 require('onedark').load()
 
+if vim.fn.has('win64') == 1 then
+	require 'nvim-treesitter.install'.prefer_git = false
+	require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
+end
+
 if vim.fn.has('wsl') == 1 then
   vim.g.clipboard = {
   name = "win32yank-wsl",
@@ -16,7 +21,4 @@ if vim.fn.has('wsl') == 1 then
     },
   cache_enable = 0,
   }
-elseif vim.fn.has('win64') == 1 then
-	require 'nvim-treesitter.install'.prefer_git = false
-	require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
 end
